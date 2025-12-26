a2b8d585e56ef0d869c8c0fb64cc00acf0285159
BrantySis <pasardanbranter@gmail.com>
2025-12-26 12:33:08 +0800

    Live testing

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ form.addEventListener('submit', async (e) => {
   };
 
   try {
-    const response = await fetch('https://branter.app.n8n.cloud/webhook/formData', {
+    const response = await fetch('https://branter.app.n8n.cloud/webhook-test/formData', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
